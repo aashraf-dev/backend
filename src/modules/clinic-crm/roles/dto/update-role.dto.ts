0215c4bf52ac42dd1e460053a4d33d332801d5f9
ntl-aashraf <abdullah.ashraf@nayatel.com>
2026-06-19 11:27:17 +0500

    crm - working completed till here

--- src/modules/clinic-crm/roles/dto/update-role.dto.ts
+++ src/modules/clinic-crm/roles/dto/update-role.dto.ts
@@ -1,6 +1,12 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateRoleDto } from './create-role.dto';
 
 export class UpdateRoleDto extends PartialType(
   OmitType(CreateRoleDto, ['permissionIds'] as const),
-) {}
+) {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
